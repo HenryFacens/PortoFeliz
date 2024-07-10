3f26c3d9d83a9d9ae24b7ad34e2da1de2906082f
Henry <hsanturiao@gmail.com>
2024-07-10 09:42:24 -0300

    feat: Create a superUser ViewSet, add Authenticated

--- back_end/backend/api/views.py
+++ back_end/backend/api/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.views.generic import View
 from rest_framework import viewsets
 from .models import User, Sector, Interaction, Feedback, AdminUser
-from .serializers import UserSerializer, SectorSerializer, InteractionSerializer, FeedbackSerializer, AdminUserSerializer
+from .models import Sector, Interaction, Feedback, AdminUser
+from .serializers import UserSerializer, SectorSerializer, InteractionSerializer, FeedbackSerializer, AdminUserSerializer, SuperUserSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from api import serializers as api_serializers
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
 class UserTokenSerializer(TokenObtainPairView):
     serializer_class = api_serializers.UserTokenSerializer
@@ -16,23 +19,35 @@ class IndexView(View):
 class MapView(View):
     def get(self, request):
         return render(request, 'map/base/base.html')
-    
+
+# ViewSets define the view behavior.
+class SuperUserViewSet(viewsets.ModelViewSet):
+    UserAuth = get_user_model()
+    queryset = UserAuth.objects.all()
+    serializer_class = SuperUserSerializer
+    permission_classes = [IsAuthenticated]
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
 class SectorViewSet(viewsets.ModelViewSet):
     queryset = Sector.objects.all()
     serializer_class = SectorSerializer
+    permission_classes = [IsAuthenticated]
 
 class InteractionViewSet(viewsets.ModelViewSet):
     queryset = Interaction.objects.all()
     serializer_class = InteractionSerializer
+    permission_classes = [IsAuthenticated]
 
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated]
 
 class AdminUserViewSet(viewsets.ModelViewSet):
     queryset = AdminUser.objects.all()
     serializer_class = AdminUserSerializer
+    permission_classes = [IsAuthenticated]
