21f8ec38681fafc273ab0db6ee6c8b08d1a5e334
Henry <hsanturiao@gmail.com>
2024-07-10 09:43:01 -0300

    fix: Change urls local API

--- back_end/backend/api/urls.py
+++ back_end/backend/api/urls.py
@@ -1,9 +1,39 @@
-from django.contrib import admin
 from django.urls import path, include
-from api.views import UserTokenSerializer
+from drf_yasg.views import get_schema_view
+from api import views
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Cidadania e Transparência API",
+      default_version='v1',
+      description="Documentação da API para o sistema de Cidadania e Transparência",
+      contact=openapi.Contact(email="h3nry.almeida@gmail.com"),
+      license=openapi.License(name="Presturion License"),
+   ),
+   public=False,
+   permission_classes=(AllowAny,),
+)
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'userAuth', views.SuperUserViewSet,basename='userauth')
+router.register(r'sectors', views.SectorViewSet)
+router.register(r'interactions', views.InteractionViewSet)
+router.register(r'feedbacks', views.FeedbackViewSet)
+router.register(r'adminusers', views.AdminUserViewSet)
 
 urlpatterns = [
-    path('user/token/', UserTokenSerializer.as_view(), name='token_obtain_pair'),
-    ]
+    path('', include(router.urls)),
 
+    # Generate Token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # Documentation
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
--- back_end/backend/backend/urls.py
+++ back_end/backend/backend/urls.py
@@ -1,42 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-# Api
-from api import views
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework.permissions import AllowAny
-from rest_framework import routers
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Cidadania e Transparência API",
-      default_version='v1',
-      description="Documentação da API para o sistema de Cidadania e Transparência",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contato@seudominio.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(AllowAny,),
-)
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'sectors', views.SectorViewSet)
-router.register(r'interactions', views.InteractionViewSet)
-router.register(r'feedbacks', views.FeedbackViewSet)
-router.register(r'adminusers', views.AdminUserViewSet)
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # API
-    path('api/', include('api.urls')),
     path('', include('frontend.urls')),
-    path('apiv2/', include(router.urls)),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    # API
+    path('api/v1/', include('api.urls')),
 
 ]
 
