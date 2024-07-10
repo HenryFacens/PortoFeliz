4b582d416a8b8506e04e21ed7b5bd162d673c9a1
Henry <hsanturiao@gmail.com>
2024-07-10 09:43:33 -0300

    feat: Adding tag REST_FRAMEWORK on settings

--- back_end/backend/backend/settings.py
+++ back_end/backend/backend/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     # Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'corsheaders',
     'drf_yasg',
     
@@ -222,6 +223,15 @@ JAZZMIN_SETTINGS = {
         "sidebar_small_text": False,
         "footer_small_text": False,
     },
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 SIMPLE_JWT = {
